da8689153ae12110aa2619565b1af0b032648e06
rosita2025 <196705675+rosita2025@users.noreply.github.com>
2025-04-12 18:11:40 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div className="contact-us-container">
+            <h2>Contact Us</h2>
+            <p>Email: <a href="mailto:contact@quickrecipeskitchen.com">contact@quickrecipeskitchen.com</a></p>
+            <p>Phone: <a href="tel:+51995487759">+51995487759</a></p>
+        </div>
+    );
+};
+
+export default ContactUs;
